fc391f469f2ad0a394db5b4f2603e8bc1aba8049
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-07-20 16:18:36 -0300

    feat: adicionando Context da aplicação

---
+++ src/providers/ContextProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { GlobalProvider } from "../context/globalProvider";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function ContextProvider({ children }: Props) {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 200);
+  }, []);
+
+  if (!isReady) {
+    return null
+  }
+
+  return <GlobalProvider>{children}</GlobalProvider>;
+}
+
+export default ContextProvider;
