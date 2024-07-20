0d2201c688f0481223e73fc0f011d907470ee509
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-07-20 16:19:08 -0300

    feat: desenvolvimento inicial do componente Sidebar

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useGlobalState } from "@/context/globalProvider";
+import styled from "styled-components";
+
+const Sidebar = () => {
+  const { theme } = useGlobalState();
+  console.log(theme);
+
+  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+};
+
+const SidebarStyled = styled.nav`
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2}
+  border-radius: 1rem;
+`;
+export default Sidebar;
