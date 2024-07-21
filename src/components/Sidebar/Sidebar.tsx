053f41b32f2d71bd79a09bdc0fc4b2deb768daec
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-07-20 21:12:18 -0300

    feat: desenvolvendo a sidebar e mapeando as rotas

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,58 @@
 "use client";
 
 import { useGlobalState } from "@/context/globalProvider";
+import Image from "next/image";
 import styled from "styled-components";
+import menu from "@/utils/menu";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
   const { theme } = useGlobalState();
-  console.log(theme);
+  const router = useRouter();
+  const pathname = usePathname();
 
-  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
+
+  return (
+    <SidebarStyled theme={theme}>
+      <div className="profile">
+        <div className="profile-overlay"></div>
+        <div className="image">
+          <Image
+            width={70}
+            height={70}
+            src="/images/avatar1.png"
+            alt="Avatar Logo"
+          />
+        </div>
+        <h1>
+          <span>Matheus</span>
+          <span>Duarte</span>
+        </h1>
+      </div>
+      <ul className="nav-items">
+        {menu.map((item) => {
+          const link = item.link;
+
+          return (
+            <li
+              className={`nav-item ${pathname === link ? "active" : ""}`}
+              onClick={() => {
+                handleClick(link);
+              }}
+            >
+              {item.icon}
+              <Link href={link}>{item.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </SidebarStyled>
+  );
 };
 
 const SidebarStyled = styled.nav`
